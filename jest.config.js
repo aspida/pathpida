ec8727f308a4089e6f2392c5056a76955aa82237
m-mitsuhide <genbu2011@live.jp>
2020-05-29 12:32:01 +0900

    chore: rename samples to pages

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testPathIgnorePatterns: ['samples'],
-  coveragePathIgnorePatterns: ['samples', 'dist']
+  testPathIgnorePatterns: ['pages'],
+  coveragePathIgnorePatterns: ['pages', 'dist']
 }
