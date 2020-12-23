8299b6145baaff803711187f91c8edf4214e4600
solufa <solufa2020@gmail.com>
2020-12-24 06:54:21 +0900

    chore: fix getConfig path

--- src/getConfig.ts
+++ src/getConfig.ts
@@ -12,7 +12,7 @@ export default (dir = process.cwd()): Config => {
   const nuxtjsPath = path.join(dir, 'nuxt.config.js')
   const nuxttsPath = path.join(dir, 'nuxt.config.ts')
   const nextLibPath = path.join(dir, 'lib')
-  const inputPath = path.join(dir, 'pages')
+  const inputPath = path.posix.join(dir, 'pages')
   const type = fs.existsSync(nuxtjsPath) || fs.existsSync(nuxttsPath) ? 'nuxtjs' : 'nextjs'
   if (type === 'nextjs') {
     if (!fs.existsSync(nextLibPath)) fs.mkdirSync(nextLibPath)
@@ -26,7 +26,7 @@ export default (dir = process.cwd()): Config => {
     return {
       type: 'nuxtjs',
       input: inputPath,
-      output: path.join(dir, 'plugins'),
+      output: path.posix.join(dir, 'plugins'),
       trailingSlash: config.trailingSlash
     }
   }
