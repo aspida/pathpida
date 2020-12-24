08f6cce9a0e4b139d81d8b83bda88dad4500d689
solufa <solufa2020@gmail.com>
2020-12-24 21:34:19 +0900

    chore: fix test

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -19,10 +19,10 @@ describe('cli test', () => {
   test('main', () => {
     fs.readdirSync('projects').forEach(dir => {
       const basePath = `projects/${dir}`
-      const { type, input, output, trailingSlash } = getConfig(basePath)
+      const { type, input, staticDir, output, trailingSlash } = getConfig(true, basePath)
 
       const result = fs.readFileSync(`${output}/$path.ts`, 'utf8')
-      const { filePath, text } = build({ type, input, output, trailingSlash })
+      const { filePath, text } = build({ type, input, staticDir, output, trailingSlash })
 
       expect(filePath).toBe(`${output}/$path.ts`)
       expect(text.replace(new RegExp(`${basePath}/`, 'g'), '')).toBe(result.replace(/\r/g, ''))
