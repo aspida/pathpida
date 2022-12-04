4ba6dd5016f6a86d8c82da30fe6c83d77fbc3663
Yo Iwamoto <you.iwamoto918@gmail.com>
2022-12-04 15:03:32 +0900

    add: project for testing nextjs app dir inside src dir

---
+++ projects/nextjs-src-appdir/src/app/%E6%97%A5%E6%9C%AC%E8%AA%9E/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <h1>page</h1>
+}
+
+export default Page
---
+++ projects/nextjs-src-appdir/src/out/lib/static/$path.ts
@@ -0,0 +1,66 @@
+import type { Query as Query0 } from '../../../app/page'
+import type { OptionalQuery as OptionalQuery1 } from '../../../app/(group1)/[pid]/page'
+import type { Query as Query2 } from '../../../app/(group1)/blog/[...slug]/page'
+
+export const pagesPath = {
+  "%E6%97%A5%E6%9C%AC%E8%AA%9E": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/%E6%97%A5%E6%9C%AC%E8%AA%9E' as const, hash: url?.hash })
+  },
+  _pid: (pid: string | number) => ({
+    $url: (url?: { query?: OptionalQuery1, hash?: string }) => ({ pathname: '/[pid]' as const, query: { pid, ...url?.query }, hash: url?.hash })
+  }),
+  "blog": {
+    _slug: (slug: string[]) => ({
+      $url: (url: { query: Query2, hash?: string }) => ({ pathname: '/blog/[...slug]' as const, query: { slug, ...url.query }, hash: url.hash })
+    }),
+    "hoge": {
+      _fuga: (fuga?: string[]) => ({
+        $url: (url?: { hash?: string }) => ({ pathname: '/blog/hoge/[[...fuga]]' as const, query: { fuga }, hash: url?.hash })
+      })
+    }
+  },
+  "aaa": {
+    _bbb: (bbb: string[]) => ({
+      "ccc": {
+        $url: (url?: { hash?: string }) => ({ pathname: '/aaa/[...bbb]/ccc' as const, query: { bbb }, hash: url?.hash })
+      }
+    }),
+  },
+  "x": {
+    _y: (y: string | number) => ({
+      "z": {
+        $url: (url?: { hash?: string }) => ({ pathname: '/x/[y]/z' as const, query: { y }, hash: url?.hash })
+      },
+      $url: (url?: { hash?: string }) => ({ pathname: '/x/[y]' as const, query: { y }, hash: url?.hash })
+    }),
+    $url: (url?: { hash?: string }) => ({ pathname: '/x' as const, hash: url?.hash })
+  },
+  "_ignore": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/.ignore' as const, hash: url?.hash })
+  },
+  _a: (a: string | number) => ({
+    "b": {
+      _c: (c: string[]) => ({
+        $url: (url?: { hash?: string }) => ({ pathname: '/[a]/b/[...c]' as const, query: { a, c }, hash: url?.hash })
+      })
+    }
+  }),
+  $url: (url: { query: Query0, hash?: string }) => ({ pathname: '/' as const, query: url.query, hash: url.hash })
+}
+
+export type PagesPath = typeof pagesPath
+
+export const staticPath = {
+  aa_json: '/aa.json',
+  bb: {
+    _ignore: '/bb/.ignore',
+    cc_png: '/bb/cc.png'
+  },
+  duplicate_json_0: {
+    sample_json: '/duplicate-json/sample.json'
+  },
+  duplicate_json_1: '/duplicate.json',
+  duplicate_json_2: '/duplicate_json'
+} as const
+
+export type StaticPath = typeof staticPath
